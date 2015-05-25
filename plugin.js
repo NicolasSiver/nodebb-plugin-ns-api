35db88856839e88b11ad9aa1e390d84d52673522
Nicolas Siver <nicolas@siver.im>
2015-05-25 22:37:22 +0300

    plugin entry

---
+++ plugin.js
@@ -0,0 +1,11 @@
+(function (Plugin) {
+
+    //NodeBB list of Hooks: https://github.com/NodeBB/NodeBB/wiki/Hooks
+    Plugin.hooks = {
+        statics: {
+            load: function (params, callback) {
+            }
+        }
+    };
+
+})(module.exports);
